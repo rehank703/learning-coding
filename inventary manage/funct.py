fd5e5217926e8a1c746b52d6f406174cc890264d
rehank703 <rehanamaankhanh@gmail.com>
2026-03-21 22:13:28 +0530

    Delete inventary manage directory

--- inventary manage/funct.py
+++
@@ -1,8 +0,0 @@
-#to add product
-def addpro(product, price, quantit):
-    print( "PRODUCT: ", product,"\nPRICE: ", price,"\nQUANTITY:", quantit)
-    print("product added succesfully") 
-    
-
-# to update the product
-
